6a1bb153621484f7390534f092b83bf3312d0b67
nwntaspap <nwntaspapmichael@gmail.com>
2025-09-03 17:25:15 +0300

    implement part of the model, add initial state and some behavior

--- battleship.js
+++ battleship.js
@@ -1,3 +1,42 @@
+let model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [
+    { locations: ["06", "16", "26"], hits: ["", "", ""] },
+    { locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] },
+  ],
+
+  fire(guess) {
+    for (let i = 0; i < this.numShips; i++) {
+      let ship = this.ships[i];
+      let index = ship.locations.indexOf(guess);
+
+      if (index >= 0) {
+        ship.hits[index] = "hits";
+        view.displayHit(guess);
+        view.displayMessage("HIT!");
+
+        if (this.isSunk(ship)) {
+          this.shipsSunk++;
+          view.displayMessage("You sank my battleship!");
+        }
+
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("MISS!");
+    return false;
+  },
+  isSunk(ship) {
+    return !ship.hits.includes("");
+  },
+};
+
 let view = {
   displayMessage(msg) {
     let messageArea = document.getElementById("messageArea");
