ed345a9249017f4e016a4d99623c64df14b576f2
nwntaspap <nwntaspapmichael@gmail.com>
2025-09-03 17:58:01 +0300

    add controller logic, get and validate user input

--- battleship.js
+++ battleship.js
@@ -51,3 +51,48 @@ let view = {
     cell.setAttribute("class", "miss");
   },
 };
+
+let controller = {
+  guesses: 0,
+
+  processGuess(guess) {
+    let location = parseGuess(guess);
+    if (location) {
+      this.guesses++;
+
+      let hit = model.fire(location);
+      if (hit && model.shipsSunk === model.numShips) {
+        view.displayMessage(
+          `You sank all my battleships, in ${this.guesses} guesses`
+        );
+      }
+    }
+  },
+};
+
+function parseGuess(guess) {
+  const alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+  if (guess === null || guess.length !== 2) {
+    alert("Oops, please enter a letter and a number on the board.");
+  } else {
+    let firstChar = guess.charAt(0);
+    let row = alphabet.indexOf(firstChar);
+    let column = guess.charAt(1);
+
+    if (isNaN(column)) {
+      alert("Oops, that isn't on the board.");
+    } else if (
+      row < 0 ||
+      row >= model.boardSize ||
+      column < 0 ||
+      column >= model.boardSize
+    ) {
+      alert("Oops, that's off the board!");
+    } else {
+      return row + column;
+    }
+  }
+
+  return null;
+}
